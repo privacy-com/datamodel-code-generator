fb2bbcb4ffb0e02cd46c72165ffb1126967da79c
luca-knaack-webcom <luca.knaack@web-computing.de>
2024-06-26 18:30:12 +0200

    feature: dots in paths (#2008)
    * sanitize dots in path names
    
    * add flag that sanitizes dots in filenames as modules
    
    * add missing __init__.py files when creating submodules.
    
    * add test
    
    * fix and extend tests
    
    * remove unnecessary list comprehension
    
    * remove unnecessary list comprehension
    
    * fix set comprehension
    
    * remove unrelated change
    
    * extract inner function for testing
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * extract classmethods to try and fix tests
    
    * Revert "extract classmethods to try and fix tests"
    
    This reverts commit bdbfec705ecce8c02e8e5ebc81066eb4f2e91f31.
    
    * Revert "extract inner function for testing"
    
    This reverts commit 20b694e813e5443ae3251518a683f17ad08465b0.
    
    * write functional test
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * increase test coverage
    
    * fix test
    
    ---------
    
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>
    Co-authored-by: Koudai Aono <koxudaxi@gmail.com>

--- datamodel_code_generator/parser/openapi.py
+++ datamodel_code_generator/parser/openapi.py
@@ -223,6 +223,7 @@ class OpenAPIParser(JsonSchemaParser):
         use_pendulum: bool = False,
         http_folder_output: Optional[Path] = None,
         http_query_parameters: Optional[Sequence[Tuple[str, str]]] = None,
+        treat_dots_as_module: bool = False,
         use_exact_imports: bool = False,
     ):
         super().__init__(
@@ -292,6 +293,7 @@ class OpenAPIParser(JsonSchemaParser):
             use_pendulum=use_pendulum,
             http_folder_output=http_folder_output,
             http_query_parameters=http_query_parameters,
+            treat_dots_as_module=treat_dots_as_module,
             use_exact_imports=use_exact_imports,
         )
         self.open_api_scopes: List[OpenAPIScope] = openapi_scopes or [
